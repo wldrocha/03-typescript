985bcd46d4de63f3cb336dbff1b57570c69f3924
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-07 17:00:52 -0400

    feat: 🎉 first commit
    git ignore, package json base and app.ts

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+const heroes = [
+  { id: 1, name: 'Batman', realName: 'Bruce Wayne' },
+  { id: 2, name: 'Superman', realName: 'Clark Kent' },
+  { id: 3, name: 'Wonder Woman', realName: 'Diana Prince' }
+]
+
+const findHero = (id: number) => heroes.find(hero => hero.id === id)
+
+const hero = findHero(4)
+
+console.log(hero?.name ?? 'Hero not found')
